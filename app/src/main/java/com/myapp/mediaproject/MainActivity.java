2a10796f921d3cf6aee6fb861a6c82ab75f71f4b
RamtinE79 <ramtin.ehsani79@gmail.com>
2020-04-06 14:12:59 +0430

    FirebaseAuth,Drawer,Notif added

--- app/src/main/java/com/myapp/mediaproject/MainActivity.java
+++ app/src/main/java/com/myapp/mediaproject/MainActivity.java
@@ -1,34 +1,150 @@
 package com.myapp.mediaproject;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.myapp.mediaproject.adapters.FragAdapter;
 
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     ViewPager viewPager;
     TabLayout tabLayout;
     EditText search;
+    DrawerLayout drawerLayout;
+    ImageView nav_img;
+    NavigationView navigationView;
+
+    public static final String ANONYMOUS = "anonymous";
+    public static final int RC_SIGN_IN = 1;
+    private String mUsername;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initialize Firebase
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
         init();
         handle();
 
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    signInInitialize(mUsername);
+                }
+                else {
+                    signOutInitialize();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build()
+                                    ,new AuthUI.IdpConfig.EmailBuilder().build()))
+                            .setIsSmartLockEnabled(false)
+                            .setLogo(R.mipmap.ic_launcher)
+                            .setTheme(R.style.LoginTheme)
+                            .build(),
+                            RC_SIGN_IN
+                    );
+                }
+            }
+        };
+
+    }
+
+    void init(){
+        viewPager = findViewById(R.id.viewPage);
+        tabLayout = findViewById(R.id.tabLay);
+        search = findViewById(R.id.edt_search);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        nav_img = findViewById(R.id.img_menu);
+    }
+
+    void handle(){
+        FragAdapter fragAdapter = new FragAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(fragAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        DrawerLayout.LayoutParams params = new DrawerLayout.LayoutParams(
+                DrawerLayout.LayoutParams.WRAP_CONTENT,
+                DrawerLayout.LayoutParams.MATCH_PARENT,GravityCompat.START
+        );
+        params.setMargins(0, getStatusBarHeight()+8, 0,0 );
+        navigationView.setLayoutParams(params);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.signOut){
+                    AuthUI.getInstance().signOut(getBaseContext());
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                return false;
+            }
+        });
+        nav_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mAuthStateListener != null)
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN){
+            if (resultCode==RESULT_OK){
+                Toast.makeText(this, "Signed in", Toast.LENGTH_SHORT).show();
+            }
+            else if (resultCode == RESULT_CANCELED){
+                finish();
+            }
+        }
     }
 
     @Override
@@ -48,16 +164,21 @@ public class MainActivity extends AppCompatActivity {
         return super.dispatchTouchEvent( event );
     }
 
-    void init(){
-        viewPager = findViewById(R.id.viewPage);
-        tabLayout = findViewById(R.id.tabLay);
-        search = findViewById(R.id.edt_search);
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 
-    void handle(){
-        FragAdapter fragAdapter = new FragAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(fragAdapter);
-        tabLayout.setupWithViewPager(viewPager);
+    private void signOutInitialize() {
+        mUsername = ANONYMOUS;
+    }
+
+    private void signInInitialize(String mUsername) {
+        this.mUsername = mUsername;
     }
 
 
